86f6a26a0ecc38d8ff903c33ae898e1a24604c1f
juanphdz <juanphdez@gmail.com>
2013-10-11 17:44:07 -0700

    commit

--- RockPaperScissorsHWW5/src/RockPaperScissors.java
+++ RockPaperScissorsHWW5/src/RockPaperScissors.java
@@ -8,16 +8,24 @@ public class RockPaperScissors {
 
 	public static void main (String[]args)
 	{
+		
+		// variables
 		int playerWinsCounter = 0;
 		int computerWinsCounter = 0;
 		int playerComputerTies = 0;
 		int games = 1;
 		int round = 1;
 		
+		int userMoveNo = 0;
+		int computerMoveNo = 0;
+		int userMoves [] = new int[100];
+		int computerMoves [] = new int[100];
 		
 		String gamesString = 
 				JOptionPane.showInputDialog("How many games to do you want to play?");
 		games = Integer.parseInt(gamesString);
+		
+		// game play process
 		
 		while (round <= games)
 		{
@@ -29,6 +37,7 @@ public class RockPaperScissors {
 		String playerAString =
 				JOptionPane.showInputDialog("\nPlayer, enter 1 for Rock, 2 for Paper, or 3 for Scissors: ");
 		int playerA = Integer.parseInt(playerAString);
+		
 		
 		if (playerA == playerComputer)
 			{String messageOne = String.format("It's a tie!!");
@@ -65,10 +74,17 @@ public class RockPaperScissors {
 			JOptionPane.showMessageDialog(null, messageSeven);
 			playerWinsCounter = playerWinsCounter + 1;}
 		
+		// array
+		 userMoves[userMoveNo] = playerA;
+		 computerMoves[computerMoveNo] = playerComputer;
+		 userMoveNo++;
+		 computerMoveNo++;
+	
+		
 		} // end while
 		
 		
-		
+		// output messages
 		if (playerWinsCounter > computerWinsCounter)
 		{String playerWinsMessage = String.format("\nPlayer wins this match");
 			JOptionPane.showMessageDialog(null, playerWinsMessage);}
@@ -84,6 +100,20 @@ public class RockPaperScissors {
 		String totalWins = String.format("\nPlayer wins: %d\nComputer wins: %d\nTies: %d\n", 
 				playerWinsCounter, computerWinsCounter, playerComputerTies);
 		JOptionPane.showMessageDialog(null, totalWins);
+		
+		
+		// array output
+		
+		for ( userMoveNo = 0; userMoveNo < userMoves.length; userMoveNo++){
+		System.out.println("User moves: " + userMoves[userMoveNo]);
+		}
+		
+		for ( computerMoveNo = 0; computerMoveNo < computerMoves.length; computerMoveNo++){
+			System.out.println("Computer moves: " + computerMoves[computerMoveNo]);
+		}
+		
+		
+		
 }
 }
 	
