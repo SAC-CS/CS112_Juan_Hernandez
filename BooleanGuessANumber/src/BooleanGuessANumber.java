4e491fd69f5bd08f556485d7d4cac417b42a2f78
juanphdz <juanphdez@gmail.com>
2013-10-14 10:22:13 -0700

    commit

--- BooleanGuessANumber/src/BooleanGuessANumber.java
+++ BooleanGuessANumber/src/BooleanGuessANumber.java
@@ -10,56 +10,65 @@ public class BooleanGuessANumber {
 	public static void main(String[] args)
 		{
 		
-		int playCount = 3;
-		boolean proceed = true;
-		while (proceed){
-		playCount ++;
-		proceed = playCount < 3;
-		
-		int x = 0 + (int) ( Math.random() * 10 );
+		// input
 		Scanner input = new Scanner( System.in );
-
-		System.out.println("Guess a number from 0-9: ");
+		int x = 0 + (int) ( Math.random() * 10 );
 		
+		int playCount = 0;
+		int playerWins = 0;
+		int computerWins = 0;
+		int userMoveNo = 0;
+		int computerMoveNo = 0;
+		int userMoves [] = new int[25];
+		int computerMoves [] = new int[25];
 		
+		System.out.println("Guess a number from 0-9 or if you want to stop playing type <Ctrl> z");
 		
-
+		while (input.hasNext()){
+		
 		int y = input.nextInt();
 		
+		if (x == y)
+		{System.out.println("You are right!");
+		playCount = playCount +1;
+		playerWins = playerWins + 1;}
+		else
+		if (x < y )
+		{System.out.println("Too high guess a lower number");
+		playCount = playCount +1;
+		computerWins = computerWins + 1;}
+		else
+		if (x > y)
+		{System.out.println("Too low guess a higher number");
+		playCount = playCount +1;
+		computerWins = computerWins + 1;}
 		
-		boolean win = x == y;
-		boolean lower = x < y;
+		// array
+				 userMoves[userMoveNo] = y;
+				 computerMoves[computerMoveNo] = x;
+				 userMoveNo++;
+				 computerMoveNo++;
 		
+		} // end while
 		
+		if (playCount != 0)
+		{
+			System.out.printf("\nTotal number of games tries: %d\n\n", playCount);
+		}
 		
-
-		if (win)
-		{System.out.println("You are right!");}
-			else {
-				if (lower )
-				{System.out.println("Too high guess a lower number");}
-					else {
-					{System.out.println("Too low guess a higher number");}
-					
-					System.out.println("Do you want to keep playing? yes = 1, no = 2");
-					int no = input.nextInt();
-					if (no == 2 );
-					
-					switch (no){
-					case 1: no = 1;
-					proceed = true;
-					break;
-					case 2: no = 2;
-					proceed = false;
-					break;
-					}
-					
-					
-					
-					
-				
-}
-}
-}
+		// array output
+		for ( userMoveNo = 0; userMoveNo < userMoves.length; userMoveNo++)
+		{System.out.println("User moves: " + userMoves[userMoveNo]);}
+			
+		for ( computerMoveNo = 0; computerMoveNo < computerMoves.length; computerMoveNo++)
+		{System.out.println("Computer moves: " + computerMoves[computerMoveNo]);}
+		
+		if (playerWins > computerWins)
+		{System.out.println("Player Wins\n");}
+		if (computerWins > playerWins)
+		{System.out.println("Computer Wins\n");}
+		if (playerWins == computerWins)
+		{System.out.println("It's a tie\n");}
+		
 }
 }
